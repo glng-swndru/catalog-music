ad97880ce3aad09d8c02c36115e3977b3e9223a1
Gilang <gilangswandaru27@gmail.com>
2025-01-01 23:13:32 +0800

    hapus comment

--- internal/configs/types.go
+++ internal/configs/types.go
@@ -1,18 +1,15 @@
 package configs
 
-// Config adalah struktur utama yang merepresentasikan konfigurasi aplikasi.
 type Config struct {
-	Service  Service  `mapstructure:"service"`  // Konfigurasi untuk bagian service.
-	Database Database `mapstructure:"database"` // Konfigurasi untuk bagian database
+	Service  Service  `mapstructure:"service"`
+	Database Database `mapstructure:"database"`
 }
 
-// Service adalah struktur untuk menyimpan konfigurasi terkait layanan (service).
 type Service struct {
 	Port      string `mapstructure:"port"`
 	SecretJWT string `mapstructure:"secretJWT"`
 }
 
-// Database adalah struktur utama yang menyimpan konfigurasi terkait database.
 type Database struct {
-	Host string `mapstructure:"host"` // Host database
+	Host string `mapstructure:"host"`
 }
