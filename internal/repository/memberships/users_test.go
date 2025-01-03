b9cf75b1961f17514960c06ef1b513ced276024b
Gilang <gilangswandaru27@gmail.com>
2025-01-04 00:01:09 +0800

    test: Add unit tests for GetUser function in memberships package
    - Implemented unit tests for the GetUser function using sqlmock and testify.
    - Covered success and failure scenarios for retrieving a user from the database.
    - Validated SQL query structure and mocked return values to ensure proper functionality.

--- internal/repository/memberships/users_test.go
+++ internal/repository/memberships/users_test.go
@@ -1,7 +1,9 @@
 package memberships
 
 import (
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/glng-swndru/catalog-music/internal/models/memberships"
@@ -99,6 +101,89 @@ func Test_repository_CreateUser(t *testing.T) {
 			}
 			if err := r.CreateUser(tt.args.model); (err != nil) != tt.wantErr {
 				t.Errorf("repository.CreateUser() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			assert.NoError(t, mock.ExpectationsWereMet())
+		})
+	}
+}
+
+func Test_repository_GetUser(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	assert.NoError(t, err)
+	defer db.Close()
+
+	gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}))
+	assert.NoError(t, err)
+
+	now := time.Now()
+
+	type args struct {
+		email    string
+		username string
+		id       uint
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *memberships.User
+		wantErr bool
+		mockFn  func(args args)
+	}{
+		{
+			name: "success",
+			args: args{
+				email:    "test@gmail.com",
+				username: "test",
+			},
+			want: &memberships.User{
+				Model: gorm.Model{
+					ID:        1,
+					CreatedAt: now,
+					UpdatedAt: now,
+				},
+				Email:     "test@gmail.com",
+				Username:  "test",
+				Password:  "test",
+				CreatedBy: "test@gmail.com",
+				UpdatedBy: "test@gmail.com",
+			},
+			wantErr: false,
+			mockFn: func(args args) {
+
+				mock.ExpectQuery(`SELECT \* FROM "users" .+`).WithArgs(args.email, args.username, args.id, 1).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "created_at", "updated_at", "email", "username", "password", "created_by", "updated_by"}).
+						AddRow(1, now, now, "test@gmail.com", "test", "test", "test@gmail.com", "test@gmail.com"))
+			},
+		},
+
+		{
+			name: "failed",
+			args: args{
+				email:    "test@gmail.com",
+				username: "test",
+			},
+			want:    nil,
+			wantErr: true,
+			mockFn: func(args args) {
+
+				mock.ExpectQuery(`SELECT \* FROM "users" .+`).WithArgs(args.email, args.username, args.id, 1).
+					WillReturnError(assert.AnError)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mockFn(tt.args)
+			r := &repository{
+				db: gormDB,
+			}
+			got, err := r.GetUser(tt.args.email, tt.args.username, tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("repository.GetUser() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("repository.GetUser() = %v, want %v", got, tt.want)
 			}
 			assert.NoError(t, mock.ExpectationsWereMet())
 		})
